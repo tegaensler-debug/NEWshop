971ab49f2f970e1357ef3a7e5363c5a6077dcaab
tegaensler@gmail.com <tegaensler@gmail.com>
2025-11-15 07:17:43 -0800

    Your branch is up to date with 'origin/main/sessions'.  Changes to be committed: 	modified:   action/auth.js 	modified:   app/layout.jsx 	modified:   lib/db.js 	modified:   lib/sessions.js

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,7 +5,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning>
         <header>
           <nav>
             <Link className="nav-link" href="/">Home</Link>
